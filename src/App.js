3501af309406ac19b0c1f54a3178c30230a84ae3
unknown <abdielmamani@upeu.edu.pe>
2024-06-19 10:51:56 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from  './components/Header/header.js';
 
+
+console.log(Header);
 function App() {
   return (
     <div className="App">
+      {/* {Header()} */}
+      <Header></Header>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          First step!
         </p>
         <a
           className="App-link"
@@ -15,7 +20,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprende React
         </a>
       </header>
     </div>
---
+++ src/components/Header/header.js
@@ -0,0 +1,8 @@
+import "./header.css"
+function Header (){
+    return  <header class = "header">
+                <img src="img/org.svg" alt="Org"/>
+            </header> 
+
+}
+export default Header
